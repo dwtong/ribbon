177fa31c898eb8ec7ca7ae14b16d9cf82dc00184
Dan Tong <dan@dtong.nz>
2023-03-24 09:26:41 +1300

    Implement state management with undo/redo
    Co-authored-by: Michael Williams <michael.williams@enspiral.com>

--- ribbon.lua
+++ ribbon.lua
@@ -4,137 +4,168 @@
 
 local keyboard = require "core/keyboard"
 
-MAX_WIDTH = 120
-LINE_COUNT = 6
-REDRAW_FPS = 30
-KEY_REPEAT_INIT_MS = 200
-KEY_REPEAT_MS = 100
+state = {
+  pos = {
+    line = 1,
+    col = 1
+  },
+  lines = {""},
+  keymods = {}
+}
 
-ribbon = ""
-key_repeat_clocks = {}
-screen_dirty = true
-key_actions = {}
-top_line = 1
-
-key_actions['BACKSPACE'] = function ()
-  ribbon = ribbon:sub(1, -2)
-end
-
-key_actions['ENTER'] = function ()
-  ribbon = ribbon .. '\n'
-end
-
-key_actions['UP'] = function ()
-  top_line = top_line > 1 and top_line - 1 or 1
-end
-
-key_actions['DOWN'] = function ()
-  top_line = top_line + 1
-end
+local applys = {}
+local reverts = {}
+local past = {}
+local future = {}
+local keydowns = {}
+local keyups = {}
 
 function init()
-  ribbon = read_file(_path.code.."ribbon/test.txt")
-
-  clock.run(function()
-    while true do
-      if screen_dirty then redraw() end
-      clock.sleep(1/REDRAW_FPS)
-    end
-  end)
-end
-
-function enc(e, d)
-  if e == 2 then
-    d = util.clamp(d, -1, 1)
-    top_line = top_line + d > 1 and top_line + d or 1
-    screen_dirty = true
-  end
+  redraw()
 end
 
 function redraw()
-  local baked_lines = lines()
-
   screen.clear()
   screen.level(15)
   screen.font_size(8)
 
-  for i=1, LINE_COUNT do
-    local line = baked_lines[i+top_line] or ''
-    screen.move(0, 10*i)
+  for index = 1, #state.lines do
+    local line = state.lines[index]
+    screen.move(0, 10 * index)
     screen.text(line)
   end
 
   screen.update()
-  screen_dirty = false
 end
 
-function read_file(path)
-  local file = io.open(path, "r")
-  if not file then return nil end
-  local content = file:read "*all"
-  file:close()
-  return content
+function key(k, v)
+  if v == 1 and k == 2 then
+    r()
+  end
+end
+
+function applys.insert(action)
+  local line = state.lines[action.pos.line]
+  local new_line =
+    line:sub(1, action.pos.col) .. action.char .. line:sub(action.pos.col + 1)
+
+  state.lines[action.pos.line] = new_line
+  state.pos.col = state.pos.col + 1
+end
+
+function reverts.insert(action)
+  local char = action.char
+  local pos = action.pos
+  local line = state.lines[pos.line]
+  local new_line =
+    line:sub(1, pos.col - 2) .. line:sub(pos.col)
+
+  state.lines[pos.line] = new_line
+  state.pos.col = state.pos.col - 1
+end
+
+function applys.newline(action)
+  state.lines[action.line + 1] = ""
+  state.pos.line = state.pos.line + 1
+end
+
+function reverts.newline(action)
+  state.lines[action.line + 1] = nil
+  state.pos.line = state.pos.line - 1
+end
+
+function apply(action)
+  applys[action.type](action)
+  table.insert(past, action)
+  redraw()
+end
+
+function revert(action)
+  reverts[action.type](action)
+  table.insert(future, action)
+  redraw()
+end
+
+function exec(action)
+  apply(action)
+  future = {}
+end
+
+function redo()
+  local action = table.remove(future)
+  if action then
+    apply(action)
+  end
+end
+
+function undo()
+  local action = table.remove(past)
+  if action then
+    revert(action)
+  end
 end
 
 
-function lines()
-  local pos = 1
-  local lines = {[1]=""}
-  local line = 1
+function keydowns.ENTER()
+  exec {
+    type =  "newline",
+    line = state.pos.line
+  }
+end
 
-  while pos <= ribbon:len() do
-    local char = ribbon:sub(pos, pos)
-    local nline = lines[line]..char
-    local width = screen.text_extents(nline) + 2 
+function keydowns.CTRL_Z()
+  undo()
+end
 
-    if char == "\n" then
-      line = line + 1
-      pos = pos + 1
-      lines[line] = ""
-    elseif width > MAX_WIDTH then
-      local br = 1
+function keydowns.CTRL_X()
+  redo()
+end
 
-      while char ~= " " do
-        char = ribbon:sub(pos-br, pos-br)
-        br = br + 1
-      end
+function keydowns.CTRL()
+  state.keymods.CTRL = true
+end
 
-      pos = pos - br + 2
-      lines[line] = lines[line]:sub(1, -br)
-      line = line + 1
-      lines[line] = ""
-    else
-      lines[line] = nline
-      pos = pos + 1
+function keyups.CTRL()
+  state.keymods.CTRL = false
+end
+
+function active_keymods()
+  local keymods = {}
+  for mod, is_active in pairs(state.keymods) do
+    if is_active then
+      table.insert(keymods,mod)
     end
   end
-
-  return lines
+  return keymods
 end
 
 function keyboard.code(key, value)
-  if value == 1 and key_actions[key] then
-    key_repeat_clocks[key] = repeat_key(key)
-  elseif value == 0 and key_repeat_clocks[key] then
-    clock.cancel(key_repeat_clocks[key])
-  end
-
-  screen_dirty = true
-end
-
-function keyboard.char(ch)
-  ribbon = ribbon .. ch
-  screen_dirty = true
-end
-
-function repeat_key(key)
-  return clock.run(function()
-    key_actions[key]()
-    clock.sleep(KEY_REPEAT_INIT_MS/1000)
-
-    while true do
-      key_actions[key]()
-      clock.sleep(KEY_REPEAT_MS/1000)
+  if value == 1 then
+    local key = key:gsub("LEFT", ""):gsub("RIGHT", "")
+    if keydowns[key] then
+      keydowns[key]()
     end
-  end)
+  else
+    if keyups[key] then
+      keyups[key]()
+    end
+  end
+end
+
+function keyboard.char(char)
+  if active_keymods() then
+    local keymod = table.concat(active_keymods(), "_")
+    local key = keymod.."_"..char:upper()
+    keydowns[key]()
+  else
+    exec {
+      type = 'insert',
+      char = char,
+      pos = state.pos,
+    } 
+  end
+end
+
+function r()
+  norns.script.load(norns.state.script)
 end

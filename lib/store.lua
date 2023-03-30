4477697f8cd9093b5452e2aeeed6f325caddc901
Dan Tong <dan@dtong.nz>
2023-03-31 09:56:34 +1300

    Alias store.state to state

--- lib/store.lua
+++ lib/store.lua
@@ -2,18 +2,20 @@ local text = include "ribbon/lib/text"
 
 local SCREEN_WIDTH = 110
 
-local store = {
-  state = {
-    lines = { "" },
-    brks = {},
-    keymods = {},
-    clocks = {},
-    pos = {
-      row = 1,
-      col = 1
-    }
+local store = {}
+
+local state = {
+  lines = { "" },
+  brks = {},
+  keymods = {},
+  clocks = {},
+  pos = {
+    row = 1,
+    col = 1
   }
 }
+
+store.state = state
 
 local history = {
   past = {},
@@ -40,20 +42,20 @@ local function revert(action)
 end
 
 function applies.insert(action)
-  local line = store.state.lines[action.pos.row]
+  local line = state.lines[action.pos.row]
   local new_line = text.splice(line, action.char, action.pos.col)
 
-  store.state.lines[action.pos.row] = new_line
+  state.lines[action.pos.row] = new_line
+
   rewrap_lines()
   move_pos(1, 0)
 end
 
 function reverts.insert(action)
-  local line = store.state.lines[action.pos.row]
+  local line = state.lines[action.pos.row]
   local new_line = text.remove(line, action.pos.col)
 
-  store.state.lines[action.pos.row] = new_line
-  store.state.pos.col = action.pos.col
+  state.lines[action.pos.row] = new_line
   rewrap_lines()
 end
 
@@ -68,18 +70,18 @@ function reverts.delete(action)
 end
 
 function applies.newline(action)
-  store.state.lines[action.line + 1] = ""
-  store.state.pos.row = store.state.pos.row + 1
-  store.state.pos.col = 1
+  -- state.lines[action.line + 1] = ""
+  -- state.pos.row = state.pos.row + 1
+  -- state.pos.col = 1
   rewrap_lines()
 end
 
 function reverts.newline(action)
-  store.state.lines[action.line + 1] = nil
-  store.state.pos.row = store.state.pos.row - 1
+  -- state.lines[action.line + 1] = nil
+  -- state.pos.row = state.pos.row - 1
 
-  local line_len = store.state.lines[store.state.pos.row]:len()
-  store.state.pos.col = line_len + 1
+  -- local line_len = state.lines[state.pos.row]:len()
+  -- state.pos.col = line_len + 1
   rewrap_lines()
 end
 
@@ -108,35 +110,35 @@ function store.undo()
 end
 
 function rewrap_lines()
-  local lines = store.state.lines
-  local brks = store.state.brks
-  local line = lines[store.state.pos.row]
+  local lines = state.lines
+  local brks = state.brks
+  local line = lines[state.pos.row]
 
   if text.width(line) > SCREEN_WIDTH then
     local next_lines, next_brks = text.rewrap_lines(lines, brks, SCREEN_WIDTH)
-    store.state.lines = next_lines
-    store.state.brks = next_brks
+    state.lines = next_lines
+    state.brks = next_brks
   end
 end
 
 function move_pos(col, row)
-  local current_col = store.state.pos.col
-  local current_row = store.state.pos.row
+  local current_col = state.pos.col
+  local current_row = state.pos.row
 
-  local num_rows = #store.state.lines
+  local num_rows = #state.lines
   local next_row = math.min(current_row + row, num_rows)
 
-  local next_line = store.state.lines[next_row]
+  local next_line = state.lines[next_row]
   local next_col = current_col + col
 
   while next_col > next_line:len() + 1 do
     next_col = next_col - next_line:len()
     next_row = next_row + 1
-    next_line = store.state.lines[next_row]
+    next_line = state.lines[next_row]
   end
 
-  store.state.pos.row = next_row
-  store.state.pos.col = next_col
+  state.pos.row = next_row
+  state.pos.col = next_col
 end
 
 return store

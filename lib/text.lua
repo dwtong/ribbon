89f1cd67a1bb2b0491d1f645950b2f1bccf3b62e
Dan Tong <dan@dtong.nz>
2023-03-31 16:27:25 +1300

    Break lines at spaces where possible

--- lib/text.lua
+++ lib/text.lua
@@ -19,6 +19,14 @@ function text.remove(str, index)
   return str:sub(1, index - 1) .. str:sub(index + 1)
 end
 
+function text.last_space_index(str)
+  if str:find("%s") == nil then
+    return nil
+  else
+    return str:len() - str:reverse():find("%s") + 1
+  end
+end
+
 function text.width(str)
   local space_px = 4
   local text_width_px = screen.text_extents(str)
@@ -38,42 +46,33 @@ function text.width(str)
 end
 
 function text.split_on_line_wrap(str, target_width)
-  -- TODO split at spaces rather than just character position
-  -- local str_has_spaces = text:match("%s")
-  local text_width = text.width(str)
+  if text.width(str) < target_width then
+    return { str }
+  else
+    local head = str
+    local split_at
 
-  if text_width > target_width then
-    local split_at = str:len() - 1
-    local head = str:sub(1, split_at)
-
-    while text.width(head) > target_width do
-      split_at = split_at - 1
+    repeat
+      split_at = head:len() - 1
+      local last_space_index = text.last_space_index(head:sub(1, split_at))
+      split_at = last_space_index or split_at
       head = str:sub(1, split_at)
-    end
+    until text.width(head) < target_width
 
     local tail = str:sub(split_at + 1)
     local splits = { head }
     local tail_splits = text.split_on_line_wrap(tail, target_width)
 
     for _, split in ipairs(tail_splits) do
-      local trimmed_split = text.trim(split)
-
-      if trimmed_split:len() > 0 then
-        table.insert(splits, trimmed_split)
-      end
+      table.insert(splits, split)
     end
 
+    table.insert(splits, "")
     return splits
-  else
-    return { str }
   end
 end
 
 function text.unwrap_lines(lines, brks)
-  -- print("lines", #lines)
-  -- print("brks", #brks)
-  -- assert(#lines == #brks + 1, '#lines == #breaks + 1')
-
   local next_lines = {}
 
   for line_index = 1, #lines do
@@ -98,10 +97,15 @@ function text.wrap_lines(lines, target_width)
   for line_index = 1, #lines do
     local line = lines[line_index]
     local wrapped_lines = text.split_on_line_wrap(line, target_width)
-    for _, wrapped_line in ipairs(wrapped_lines) do
+
+    for wrapped_index, wrapped_line in ipairs(wrapped_lines) do
       table.insert(next_lines, wrapped_line)
-      local brk = line_index == #wrapped_lines and LINE_BREAK or WRAP_BREAK
-      table.insert(next_brks, brk)
+
+      if wrapped_index == #wrapped_lines then
+        table.insert(next_brks, LINE_BREAK)
+      else
+        table.insert(next_brks, WRAP_BREAK)
+      end
     end
   end
 
